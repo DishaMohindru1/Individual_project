9fe67bafe46e831dfa161aa09f6e7255e796021e
PuneetKhanna18 <103989089+PuneetKhanna18@users.noreply.github.com>
2023-04-15 17:06:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const connectToMongo = require('./db');
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
-const ejs = require('ejs');
+// const mongoose = require('mongoose');
+// const ejs = require('ejs');
 connectToMongo();
 
 const app = express()
@@ -14,10 +14,10 @@ app.use(cors())
 app.use('/', require("./routes/Test3"));
 // app.use('/api/notes', require("./routes/Notes"));
 
-app.set("view engine", "ejs");
-app.get("/", (req, res) => {
- res.render("index.ejs"); // index refers to index.ejs
-});
+// app.set("view engine", "ejs");
+// app.get("/", (req, res) => {
+//  res.render("index.ejs"); // index refers to index.ejs
+// });
 
 app.listen(port, () => {
   console.log(`App running at http://localhost:${port}`)
